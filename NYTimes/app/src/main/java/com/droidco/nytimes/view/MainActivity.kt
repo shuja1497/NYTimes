6a7682c17691ad12a72646c83f9c0f240ff87096
shuja1497 <shujareshi1497@gmail.com>
2020-11-26 03:35:31 +0530

    added scroll bar to detail page

--- NYTimes/app/src/main/java/com/droidco/nytimes/view/MainActivity.kt
+++ NYTimes/app/src/main/java/com/droidco/nytimes/view/MainActivity.kt
@@ -2,26 +2,23 @@ package com.droidco.nytimes.view
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
 import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.NavigationUI
 import com.droidco.nytimes.R
+import com.droidco.nytimes.databinding.ActivityMainBinding
 import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var navView: BottomNavigationView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        val navView: BottomNavigationView = findViewById(R.id.nav_view)
+        val binding = DataBindingUtil.setContentView<ActivityMainBinding>(this, R.layout.activity_main)
+        navView = binding.navView
 
         val navController = findNavController(R.id.nav_host_fragment)
-        // Passing each menu ID as a set of Ids because each
-        // menu should be considered as top level destinations.
-//        val appBarConfiguration = AppBarConfiguration(setOf(
-//                R.id.navigation_science, R.id.navigation_business, R.id.navigation_movies))
-//        setupActionBarWithNavController(navController, appBarConfiguration)
-//        navView.setupWithNavController(navController)
-
         NavigationUI.setupWithNavController(navView, navController)
 
         navView.setOnNavigationItemReselectedListener {
